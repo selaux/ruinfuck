c22fc92a6e01d67bed1a12dd6545712f5fea8705
Stefan Lau <github@stefanlau.com>
2018-05-19 15:40:21 +0200

    Optimize to offset nodes

--- src/optimizer.rs
+++ src/optimizer.rs
@@ -101,12 +101,129 @@ impl OptimizationStep for ReplaceZeroAssignments {
     }
 }
 
+struct CollapseOffsets;
+
+impl OptimizationStep for CollapseOffsets {
+    fn apply(&self, code: Vec<Node>) -> Vec<Node> {
+        code.into_iter().fold(vec!(), move |mut acc, node| {
+            let last = acc.pop();
+            let new_node = match node {
+                Node::Conditional(body) => Node::Conditional(self.apply(body)),
+                n => n
+            };
+
+            match last {
+                Some(Node::Right(offset)) => {
+                    match new_node {
+                        Node::Inc(v, 0, false) => acc.push(Node::Inc(v, offset as i32, true)),
+                        Node::Dec(v, 0, false) => acc.push(Node::Dec(v, offset as i32, true)),
+                        Node::Assign(v, 0, false) => acc.push(Node::Assign(v, offset as i32, true)),
+                        n => {
+                            acc.push(last.unwrap());
+                            acc.push(n);
+                        }
+                    }
+                },
+                Some(Node::Left(offset)) => {
+                    match new_node {
+                        Node::Inc(v, 0, false) => acc.push(Node::Inc(v, -(offset as i32), true)),
+                        Node::Dec(v, 0, false) => acc.push(Node::Dec(v, -(offset as i32), true)),
+                        Node::Assign(v, 0, false) => acc.push(Node::Assign(v, -(offset as i32), true)),
+                        n => {
+                            acc.push(last.unwrap());
+                            acc.push(n);
+                        }
+                    }
+                },
+                Some(old_node) => {
+                    match new_node {
+                        Node::Right(right) => {
+                            match old_node {
+                                Node::Inc(value, offset, true) | Node::Dec(value, offset, true) | Node::Assign(value, offset, true) => {
+                                    if offset < 0 {
+                                        let diff = offset.abs() - right as i32;
+                                        let build_node = match old_node {
+                                            Node::Inc(_, _, _) => Node::Inc,
+                                            Node::Dec(_, _, _) => Node::Dec,
+                                            Node::Assign(_, _, _) => Node::Assign,
+                                            _ => unreachable!()
+                                        };
+
+                                        if diff > 0 {
+                                            acc.push(Node::Left(diff as u8));
+                                            acc.push(build_node(value, -(right as i32), false));
+                                        } else if diff == 0 {
+                                            acc.push(build_node(value, offset, false));
+                                        } else {
+                                            acc.push(build_node(value, offset, false));
+                                            acc.push(Node::Right(diff.abs() as u8));
+                                        }
+                                    } else {
+                                        acc.push(old_node);
+                                        acc.push(new_node);
+                                    }
+                                },
+                                _ => {
+                                    acc.push(old_node);
+                                    acc.push(new_node);
+                                }
+                            }
+                        },
+                        Node::Left(left) => {
+                            match old_node {
+                                Node::Inc(value, offset, true) | Node::Dec(value, offset, true) | Node::Assign(value, offset, true) => {
+                                    if offset > 0 {
+                                        let diff = offset - left as i32;
+                                        let build_node = match old_node {
+                                            Node::Inc(_, _, _) => Node::Inc,
+                                            Node::Dec(_, _, _) => Node::Dec,
+                                            Node::Assign(_, _, _) => Node::Assign,
+                                            _ => unreachable!()
+                                        };
+
+                                        if diff < 0 {
+                                            acc.push(build_node(value, offset, false));
+                                            acc.push(Node::Left((-diff) as u8));
+                                        } else if diff == 0 {
+                                            acc.push(build_node(value, offset, false));
+                                        } else {
+                                            acc.push(Node::Right(diff as u8));
+                                            acc.push(build_node(value, left as i32, false));
+                                        }
+                                    } else {
+                                        acc.push(old_node);
+                                        acc.push(new_node);
+                                    }
+                                },
+                                _ => {
+                                    acc.push(old_node);
+                                    acc.push(new_node);
+                                }
+                            }
+                        },
+                        _ => {
+                            acc.push(old_node);
+                            acc.push(new_node);
+                        }
+                    }
+                },
+                None => {
+                    acc.push(new_node);
+                }
+            }
+
+            acc
+        })
+    }
+}
+
 pub fn optimize_code(code: &Vec<Node>) -> Vec<Node> {
     let without_comments: Vec<Node> = FilterComments.apply(code.clone());
     let joined_operators = MergeRepeatedOperators.apply(without_comments);
     let without_zero_loops = ReplaceZeroAssignments.apply(joined_operators);
+    let with_offsets = CollapseOffsets.apply(without_zero_loops);
 
-    without_zero_loops
+    with_offsets
 }
 
 #[cfg(test)]
@@ -152,16 +269,16 @@ mod tests {
             Node::Left(1),
             Node::Right(1),
             Node::Conditional(vec!(
-                Node::Inc(1, 0, false),
+                Node::Inc(1, 1, false),
                 Node::Comment('a'),
-                Node::Inc(1, 0, false),
+                Node::Inc(1, 1, false),
                 Node::Conditional(vec!(
                     Node::Comment('a'),
                     Node::Right(1),
-                    Node::Dec(1, 0, false),
+                    Node::Dec(1, 1, false),
                     Node::Right(1),
-                    Node::Dec(1, 0, false),
-                    Node::Dec(1, 0, false),
+                    Node::Dec(1, 1, false),
+                    Node::Dec(1, 1, false),
                 ))
             ))
         );
@@ -172,12 +289,12 @@ mod tests {
             Node::Left(2),
             Node::Right(1),
             Node::Conditional(vec!(
-                Node::Inc(2, 0, false),
+                Node::Inc(2, 1, false),
                 Node::Conditional(vec!(
                     Node::Right(1),
-                    Node::Dec(1, 0, false),
+                    Node::Dec(1, 1, false),
                     Node::Right(1),
-                    Node::Dec(2, 0, false)
+                    Node::Dec(2, 1, false)
                 ))
             ))
         ));
@@ -235,6 +352,128 @@ mod tests {
             Node::Conditional(vec!(
                 Node::Assign(0, 0, false),
             ))
+        ));
+    }
+
+    #[test]
+    fn it_should_collapse_to_positive_offsets() {
+        let code = vec!(
+            Node::Right(5),
+            Node::Inc(1, 0, false),
+            Node::Right(5),
+            Node::Dec(1, 0, false),
+            Node::Right(5),
+            Node::Assign(1, 0, false),
+            Node::Conditional(vec!(
+                Node::Right(5),
+                Node::Inc(1, 0, false),
+                Node::Right(5),
+                Node::Dec(1, 0, false),
+                Node::Right(5),
+                Node::Assign(1, 0, false),
+            ))
+        );
+        let result = optimize_code(&code);
+
+        assert_eq!(result, vec!(
+            Node::Inc(1, 5, true),
+            Node::Dec(1, 5, true),
+            Node::Assign(1, 5, true),
+            Node::Conditional(vec!(
+                Node::Inc(1, 5, true),
+                Node::Dec(1, 5, true),
+                Node::Assign(1, 5, true),
+            ))
+        ));
+    }
+
+    #[test]
+    fn it_should_collapse_to_negative_offsets() {
+        let code = vec!(
+            Node::Left(5),
+            Node::Inc(1, 0, false),
+            Node::Left(5),
+            Node::Dec(1, 0, false),
+            Node::Left(5),
+            Node::Assign(1, 0, false),
+            Node::Conditional(vec!(
+                Node::Left(5),
+                Node::Inc(1, 0, false),
+                Node::Left(5),
+                Node::Dec(1, 0, false),
+                Node::Left(5),
+                Node::Assign(1, 0, false),
+            ))
+        );
+        let result = optimize_code(&code);
+
+        assert_eq!(result, vec!(
+            Node::Inc(1, -5, true),
+            Node::Dec(1, -5, true),
+            Node::Assign(1, -5, true),
+            Node::Conditional(vec!(
+                Node::Inc(1, -5, true),
+                Node::Dec(1, -5, true),
+                Node::Assign(1, -5, true),
+            ))
+        ));
+    }
+
+    #[test]
+    fn it_should_collapse_non_moving_nodes() {
+        let code = vec!(
+            Node::Left(5),
+            Node::Inc(1, 0, false),
+            Node::Right(5),
+            Node::Left(5),
+            Node::Dec(1, 0, false),
+            Node::Right(5),
+            Node::Right(5),
+            Node::Assign(1, 0, false),
+            Node::Left(5),
+            Node::Conditional(vec!(
+                Node::Left(5),
+                Node::Inc(1, 0, false),
+                Node::Right(5),
+                Node::Left(5),
+                Node::Dec(1, 0, false),
+                Node::Right(5),
+                Node::Right(5),
+                Node::Assign(1, 0, false),
+                Node::Left(5),
+            ))
+        );
+        let result = optimize_code(&code);
+
+        assert_eq!(result, vec!(
+            Node::Inc(1, -5, false),
+            Node::Dec(1, -5, false),
+            Node::Assign(1, 5, false),
+            Node::Conditional(vec!(
+                Node::Inc(1, -5, false),
+                Node::Dec(1, -5, false),
+                Node::Assign(1, 5, false),
+            ))
+        ));
+    }
+
+    #[test]
+    fn it_should_collapse_imbalanced_non_moving_nodes() {
+        let code = vec!(
+            Node::Left(7),
+            Node::Inc(1, 0, false),
+            Node::Right(5),
+            Node::Left(5),
+            Node::Inc(1, 0, false),
+            Node::Right(7)
+        );
+        let result = optimize_code(&code);
+
+        assert_eq!(result, vec!(
+            Node::Left(2),
+            Node::Inc(1, -5, false),
+            Node::Inc(1, -5, false),
+            Node::Right(2)
         ));
     }
 }
